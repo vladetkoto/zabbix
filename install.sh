03f7794ff175e5eb5ef2e36d0b7307f3256301de
vladetkoto <33867374+vladetkoto@users.noreply.github.com>
2017-12-26 13:29:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 # simple Bash script for easy system prepare by vladetkoto@gmail.com
 
-apt-get update
-apt-get -y install \
+sudo apt-get update
+sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-apt-get update && \apt-get -y install docker-ce 
+sudo apt-get update && \apt-get -y install docker-ce 
 sudo chmod +x /usr/local/bin/docker-compose
-apt-get clean
+sudo apt-get clean
 docker-compose up -d
